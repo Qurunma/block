e6d9adb178e1aa6c2dba4a8d0be94769f4bf245d
Qurunma <81317971+Qurunma@users.noreply.github.com>
2022-10-18 11:34:37 +0300

    Add files via upload

--- Переводы/geth/gethInit.bat
+++ Переводы/geth/gethInit.bat
@@ -1,6 +1,8 @@
-cd ./
-timeout /t 10
 start geth --datadir ./ --networkid 1547 --http --http.corsdomain "*" --allow-insecure-unlock --http --http.api personal,eth,net,web3
-start http://localhost:80
-geth attach --exec miner.start(1)
-geth attach
+cd ../../../
+"C:\OpenServer\Open Server.exe" /start
+"C:\Program Files\Google\Chrome\Application\chrome.exe" http://block:80
+timeout /t 10
+cd C:\OpenServer\domains\block\geth
+geth attach \\.\pipe\geth.ipc --exec miner.start(1)
+geth attach \\.\pipe\geth.ipc
